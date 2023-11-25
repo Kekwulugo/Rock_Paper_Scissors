635dbd80ff7a80fde1adea368fc009f60446cd5d
Kelechi Ekwulugo <kekwulugo@gmail.com>
2023-11-24 20:15:33 -0700

    Connect buttons to game logic and add UI

--- script.js
+++ script.js
@@ -1,64 +1,69 @@
+let computerScore = 0;
+let playerScore = 0;
+let round = 0;
+
 function getComputerChoice ()  {
   let computerChoice = Math.floor(Math.random()*3);
   return computerChoice === 0? "Rock": computerChoice === 1? "Paper": "Scissors";
 } 
 
 
-let computerScore = 0;
-let playerScore = 0;
-
 function playRound (playerSelection, computerSelection){
   computerSelection = getComputerChoice();
-  console.log(getComputerChoice());
+  round++;
   
-  playerSelection = prompt( "Rock, Paper, or Scissors?");
   if (playerSelection == computerSelection){
-    console.log("It's a tie!");
+    alert("It's a tie this round!");
     
   } else if (playerSelection == "Rock" && computerSelection == "Scissors"){
-    console.log( "You win! Rock beats Scissors");
+    alert( "You win this round! Rock beats Scissors");
     playerScore++;
     
 } else if (playerSelection == "Paper" && computerSelection == "Rock"){
-    console.log( "You win! Paper beats Rock");
+    alert( "You win this round! Paper beats Rock");
     playerScore++;
   
 } else if (playerSelection == "Scissors" && computerSelection == "Paper"){  
-    console.log("You win! Scissors beats Paper");
+    alert("You win this round! Scissors beats Paper");
     playerScore++;
     
 } else { 
-    console.log( "You lose!");
+    alert( "You lose this round!");
     computerScore++;
     
   }
 
-  return playerScore;
-  return computerScore;
+  console.log(playerScore);
+  console.log(computerScore);
+  console.log("Round: "+ round)
   }
 
-  let button = document.querySelectorAll('button');
-  let div = document.querySelector('div')
+  let buttons = document.querySelectorAll(".btn");
+  let divComp = document.querySelector("#Computer");
+  let divPlayer = document.querySelector("#Player");
+  let playerChoice = document.querySelector("#PlayerChoice")
+  let computerPick = document.querySelector("#ComputerChoice")
+  let roundCounter = document.querySelector("#Round");
 
-  button.addEventListener("click", playRound(button.value));
-
+  
+  buttons.forEach(function (button){
+    button.addEventListener("click",function(){
+      if (playerScore === 5 || computerScore ===5 && playerScore>computerScore){
+        alert("You won the game, congrats!");
+      } else if (playerScore === 5 || computerScore ===5 && playerScore<computerScore){
+        alert("Sorry you lost, better luck next time!")
+      } else {
+      playRound(button.value)
+      divPlayer.textContent = "Player Score: "+ playerScore; 
+      divComp.textContent =  "Computer Score: " + computerScore;
+      playerChoice.textContent = "You choose:  " + button.value;
+      roundCounter.textContent = "Round: " + round;
+    
+    }
+  }) 
+    }
+  );
   
 
 
 
-/*  function game () {
-  for (let i = 0; i < 5; i++){
-    playRound();
-    console.log("Player Score: "+ playerScore);
-    console.log("Computer Score: " + computerScore);
-  
-}
-  if (computerScore < playerScore){
-    console.log ("You win");
-  } else {
-    console.log ("You lose");
-  }
-}
-
-  game();
-*/
